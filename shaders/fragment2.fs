5866a1eda238b1383d39459694dddebb74346a5b
Joel <j.settrbrg@gmail.com>
2021-05-25 22:03:35 +0200

    Fix fragment shader two shifting color

--- shaders/fragment2.fs
+++ shaders/fragment2.fs
@@ -1,8 +1,8 @@
 #version 330 core
 out vec4 FragColor;
-uniform vec4 ourColor;
+uniform float ourColor;
 void main()
 {
 	FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);
-	FragColor = ourColor;
+	FragColor = vec4(0.0f, ourColor, 0.0f, 1.0f);
 }
